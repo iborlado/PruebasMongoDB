4f58d906bc48dfb7af8164428d852a65d49ca0ab
iborlado <iborlado@atsistemas.com>
2018-03-02 14:25:48 +0100

    Añade properties

--- src/main/java/com/prueba/mongo/entities/Cliente.java
+++ src/main/java/com/prueba/mongo/entities/Cliente.java
@@ -1,7 +1,9 @@
 package com.prueba.mongo.entities;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document
 public class Cliente {
 
 	@Id
